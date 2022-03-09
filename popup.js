72afd3c76e5a717baf0e984190ab7a4df9688bac
Andrew Yang <andrewhuiyang0830@gmail.com>
2022-03-08 22:31:29 -0700

    seperated sim_input

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let guess
+
 async function updateGuesses() {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
   const guesses_elem = document.getElementById("guesses")
@@ -6,7 +7,6 @@ async function updateGuesses() {
     await chrome.tabs.sendMessage(tab.id, {}, (resp) => {
       guess = resp
       guesses_elem.innerHTML = ""
-      console.log(resp)
       if (resp.length === 0) {
         const msg_elem = document.createElement("p")
         msg_elem.classList.add("message")
@@ -45,31 +45,22 @@ async function updateGuesses() {
   }
 }
 // Run when the popup is clicked
-document.addEventListener('DOMContentLoaded', async () =>
+document.addEventListener('DOMContentLoaded', async () => {
   await updateGuesses()
+  }
 )
 
 async function injectTheScript() {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    chrome.scripting.executeScript({target: {tabId: tabs[0].id}, func: sim_input, args: [this.id]})
+    // browser.tabs.executeScript({target: {tabId: tabs[0].id}, func: sim_input, args: [this.id]})
+    chrome.scripting.executeScript({
+      target: {tabId: tabs[0].id},
+      files: ["util/sim_input.js"],
+    }, _ => {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        guess: this.id
+      })
+    })
   })
   await updateGuesses()
-}
-
-function sim_input(guess) {
-  const key_map = {
-    q:[1,1],w:[1,2],e:[1,3],r:[1,4],t:[1,5],y:[1,6],u:[1,7],i:[1,8],o:[1,9],p:[1,10],
-    a:[2,2],s:[2,3],d:[2,4],f:[2,5],g:[2,6],h:[2,7],j:[2,8],k:[2,9],l:[2,10],
-    z:[3,2],x:[3,3],c:[3,4],v:[3,5],b:[3,6],n:[3,7],m:[3,8],
-    enter:[3,1]
-  }
-  for (let i = 0; i < 6; i++) {
-    let k = ''
-    if (i >= 5) {
-      k = 'enter'
-    } else {
-      k = guess[i]
-    }
-    document.querySelector("body > game-app").shadowRoot.querySelector("#game > game-keyboard").shadowRoot.querySelector(`#keyboard > div:nth-child(${key_map[k][0]}) > button:nth-child(${key_map[k][1]})`).click()
-  }
 }
